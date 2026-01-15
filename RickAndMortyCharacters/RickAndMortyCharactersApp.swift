7a9cc38012d4932127577e18887066f424c649cb
gkum4 <gustavokumasawa@gmail.com>
2026-01-14 22:17:10 -0300

    Initial Commit

---
+++ RickAndMortyCharacters/RickAndMortyCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyCharactersApp.swift
+//  RickAndMortyCharacters
+//
+//  Created by Gustavo Kumasawa on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyCharactersTests/RickAndMortyCharactersTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyCharactersTests.swift
+//  RickAndMortyCharactersTests
+//
+//  Created by Gustavo Kumasawa on 14/01/26.
+//
+
+import Testing
+@testable import RickAndMortyCharacters
+
+struct RickAndMortyCharactersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
