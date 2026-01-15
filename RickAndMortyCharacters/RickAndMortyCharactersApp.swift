cf25ca76b6eae4350ea535f3cce5ae2cc6f816a7
gkum4 <gustavokumasawa@gmail.com>
2026-01-14 22:28:30 -0300

    clean: remove unused files

--- RickAndMortyCharacters/RickAndMortyCharactersApp.swift
+++ RickAndMortyCharacters/RickAndMortyCharactersApp.swift
@@ -1,17 +1,10 @@
-//
-//  RickAndMortyCharactersApp.swift
-//  RickAndMortyCharacters
-//
-//  Created by Gustavo Kumasawa on 14/01/26.
-//
-
 import SwiftUI
 
 @main
 struct RickAndMortyCharactersApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CharactersListView()
         }
     }
 }
---
+++ RickAndMortyCharacters/Scenes/CharactersList/CharactersListView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct CharactersListView: View {
+    var body: some View {
+        Text("CharactersListView")
+    }
+}
+
+#Preview {
+    CharactersListView()
+}
